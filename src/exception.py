0609ef98d69e57a65f8fa23dd32813d838d34702
vasumylapalli2005 <vasumylapalli22@gmail.com>
2026-01-24 01:08:58 +0530

    EDA and Model training

--- src/exception.py
+++ src/exception.py
@@ -17,5 +17,5 @@ class CustomeException(Exception):
     super().__init__(error_message)
     self.error_message = error_message_detail(error_message, error_detail = error_detail)
 
-  def __srt__(self):
+  def __str__(self):
     return self.error_message
